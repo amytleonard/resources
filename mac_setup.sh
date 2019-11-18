44c164a248f62ce7b021c0783371c68bd808300e
Amy Leonard <amytleonard@users.noreply.github.com>
2019-11-18 13:17:48 -0600

    Create mac_setup.sh

---
+++ mac_setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Bootstrap brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+git -C "$(brew --repo homebrew/core)" fetch --unshallow
+
+# Basic Brew workstation packages
+brew install git
+brew tap drone/drone
+# CI / CM
+brew install drone ansible consul vault
+# Basic utilities
+brew install curl wget zsh zsh-completions bash-completion zsh-syntax-highlighting jq
+# Cloud runtimes
+brew install kubernetes-cli azure-cli
+
+# Brew Casks
+brew cask install virtualbox
+brew cask install docker
+brew cask install vagrant
+brew cask install hammerspoon
+brew cask install atom
+brew cask install clipy
+brew cask install skitch
+brew cask install wireshark
+
+# Atom Stuff
+apm install atom-beautify
+apm install file-icons
+apm install ansible-vault
+apm install linter-ansible-linting
+apm install language-ansible
+apm install autocomplete-ansible
+apm install git-plus
+apm install git-time-machine
+apm install linter-terraform-syntax
+apm install language-terraform
+apm install sublime-style-column-selection
+
+# Execute kinit for the first time
+echo Enter password to initialize kinit:
+kinit
